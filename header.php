0f553c523b5c05522e9352b85934e98f4ba3d333
h-takahashi0422 <h-takahashi@mighty2.com>
2021-06-11 20:33:38 +0900

    ざっくり静的要素流し込み

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <meta name="description">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/reset.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
+	<?php wp_head(); ?>
+</head>
+<body>
+    <div class="wrapper">
+        <header>
+            <h1><img src="" alt=""></h1>
+            <div class="m-hm">
+                <span class=""></span>
+                <span class=""></span>
+                <span class=""></span>
+            </div>
+            <nav>
+                <ul class="e-menuList">
+                    <li class="e-menuListItem"><a href="">事例</a></li>
+                    <li class="e-menuListItem"><a href="">選ばれる理由</a></li>
+                    <li class="e-menuListItem"><a href="">解決までの流れ</a></li>
+                    <li class="e-menuListItem"><a href="">費用</a></li>
+                    <li class="e-menuListItem"><a href="">よくあるご質問</a></li>
+                    <li class="e-menuListItem"><a href="">無料相談</a></li>
+                </ul>
+            </nav>
+        </header>
