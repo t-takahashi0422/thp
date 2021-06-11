c5adc41d3d3bd6af96f9f31c40fa2fec5d990116
EC2 Default User <ec2-user@ip-172-31-8-228.us-west-2.compute.internal>
2021-06-11 05:16:32 +0000

    フロントページ実装

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+	<?php wp_head(); ?>
+</head>
+<body>
+ 
+
+   	<?php wp_footer(); ?> 
+</body>
+</html>
